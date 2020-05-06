cbaa37db0376c3f9994efe6bf1465507ada6ae7e
Andrei <Muiedinamo97>
2020-05-06 10:50:19 +0300

    recycler view created

---
+++ app/src/main/java/com/andreimargaritoiu/newsreader/bindings/RecylerBindings.java
@@ -0,0 +1,24 @@
+package com.andreimargaritoiu.newsreader.bindings;
+
+import androidx.databinding.BindingAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.andreimargaritoiu.newsreader.adapter.ArticleAdapter;
+import com.andreimargaritoiu.newsreader.model.ArticleItemViewModel;
+import java.util.List;
+
+public class RecylerBindings {
+
+    @BindingAdapter({"items"})
+    public static void addFeedItems(RecyclerView recyclerView, List<ArticleItemViewModel> articles) {
+        ArticleAdapter articleAdapter = (ArticleAdapter) recyclerView.getAdapter();
+
+        if (articleAdapter == null) {
+            articleAdapter = new ArticleAdapter();
+            recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+            recyclerView.setAdapter(articleAdapter);
+        }
+
+        articleAdapter.setItems(articles);
+    }
+}
---
+++ app/src/main/java/com/andreimargaritoiu/newsreader/fragment/NewsListFragment.java
@@ -0,0 +1,40 @@
+package com.andreimargaritoiu.newsreader.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import com.andreimargaritoiu.newsreader.databinding.ArticleListFragmentBinding;
+import com.andreimargaritoiu.newsreader.model.NewsListViewModel;
+
+public class NewsListFragment extends Fragment {
+
+    private NewsListViewModel viewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        viewModel = ViewModelProviders.of(requireActivity()).get(NewsListViewModel.class);
+
+        //for those lifecycle callbacks in view model, like ON_CREATE
+        getLifecycle().addObserver(viewModel);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        ArticleListFragmentBinding binding = ArticleListFragmentBinding
+                .inflate(inflater, container, false);
+
+        binding.setViewModel(viewModel);
+
+        return binding.getRoot();
+    }
+}
